8fabdaff9e9c2400a21bcabec6071ef44c8af83f
Geilson Santos <geilsonsantos084@gmail.com>
2024-08-19 10:53:35 -0300

    tela cadastro

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-registerBtn.addEventListener('click', () => { 
-    container.classList.add("active");
+// Adicione a classe para iniciar com a tela de cadastro visível
+container.classList.add("sign-up-mode");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
 });
 
-loginBtn.addEventListener('click', () => {
-     container.classList.remove("active");
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
 });
