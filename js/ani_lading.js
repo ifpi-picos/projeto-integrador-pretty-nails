3a0b279355a7501053aba6cabd34ab010c8c8057
Nailan Nobre <nailannobre22@gmail.com>
2024-09-24 22:31:43 -0300

    Animações-lading-page

---
+++ js/ani_lading.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    ScrollReveal().reveal('.anipag1', {
+        origin: 'top',
+        easing: 'ease-out',
+        duration: 1000,
+        distance: '40%',
+    });
+
+    ScrollReveal().reveal('.topico1', {
+        origin: 'left',
+        easing: 'ease-out',
+        duration: 700,
+        distance: '40%'
+    });
+
+    ScrollReveal().reveal('.topico2', {
+        origin: 'right',
+        easing: 'ease-out',
+        duration: 700,
+        distance: '40%'
+    });
+
+    ScrollReveal().reveal('.topico3', {
+        origin: 'left',
+        easing: 'ease-out',
+        duration: 700,
+        distance: '40%'
+    });
+
+});
