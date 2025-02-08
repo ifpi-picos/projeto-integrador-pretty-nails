5115fc1bb16e295f7a80ec148aa59af4940c5947
Nailan Nobre <nailannobre22@gmail.com>
2025-02-08 20:45:00 -0300

    Tela de cadastro grow up

--- js/login.js
+++
@@ -1,8 +0,0 @@
-
-//função de login para cadastro e vice-versa
-const signUpButton = document.getElementById('signUp');
-const container = document.querySelector('.container');
-
-signUpButton.addEventListener('click', () => {
-    container.classList.toggle('right-panel-active');
-});
