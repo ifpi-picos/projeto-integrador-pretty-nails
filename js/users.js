8e9feb4c3e2a294d260a339f5dcf415239ad08e5
Nailan Nobre <nailannobre22@gmail.com>
2025-02-12 21:20:01 -0300

    mesma coisa do commit passado só que teste3

--- js/users.js
+++ js/users.js
@@ -1,4 +1,3 @@
-// FUNÇÃO PARA ADICIONAR USUÁRIO
 function adicionarUsuario() {
   const campoNome = document.querySelector("#nome").value;
   const campoEmail = document.querySelector("#email").value;
@@ -7,33 +6,32 @@ function adicionarUsuario() {
   const campoEstado = document.querySelector("#estado").value;
   const campoCidade = document.querySelector("#cidade").value;
   const campoTipo = document.querySelector("#tipo").value;
-  const campoFoto = document.querySelector("#imagem").files[0]; // Obtém o arquivo de imagem
+  const campoFoto = document.querySelector("#imagem").files[0];
 
   const formData = new FormData();
-  formData.append('name', campoNome);
-  formData.append('email', campoEmail);
-  formData.append('password', campoSenha);
-  formData.append('telefone', campoTelefone);
-  formData.append('estado', campoEstado);
-  formData.append('cidade', campoCidade);
-  formData.append('tipo', campoTipo);
-  formData.append('foto', campoFoto); // Adiciona a imagem ao FormData
+  formData.append("name", campoNome);
+  formData.append("email", campoEmail);
+  formData.append("password", campoSenha);
+  formData.append("telefone", campoTelefone);
+  formData.append("estado", campoEstado);
+  formData.append("cidade", campoCidade);
+  formData.append("tipo", campoTipo);
+  formData.append("foto", campoFoto);
 
-  fetch('https://back-end-u9vj.onrender.com/signup', {
-  method: 'POST',
-  body: formData,
-  headers: {
-    // NÃO definir 'Content-Type', pois o navegador já define automaticamente para `multipart/form-data`
-  }
-}).then(response => response.json())
-  .then(data => {
-    console.log("Usuário cadastrado com sucesso: ", data);
-    window.location.href = 'principal.html';
+  fetch("https://back-end-u9vj.onrender.com/signup", {
+    method: "POST",
+    body: formData,
   })
-  .catch(error => {
-    console.log("Erro ao cadastrar usuário: ", error);
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Usuário cadastrado com sucesso: ", data);
+      window.location.href = "principal.html";
+    })
+    .catch((error) => {
+      console.log("Erro ao cadastrar usuário: ", error);
     });
-  }
+}
+
 
 
 //FUNÇÃO PARA FAZER LOGIN
