0971639a929a29fdabcb8358fb69bd9273271751
Nailan-Nobre <nailannobre22@gmail.com>
2025-10-01 11:26:11 -0300

    Temas versão alpha

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
- window.API_BASE_URL = "http://localhost:3000";
-//  window.API_BASE_URL = "https://back-end-jf0v.onrender.com";
+//  window.API_BASE_URL = "http://localhost:3000";
+ window.API_BASE_URL = "https://back-end-jf0v.onrender.com";
 //  window.API_BASE_URL = "https://backend-production-044b.up.railway.app";
 
 // Configuração do domínio frontend para compartilhamento
